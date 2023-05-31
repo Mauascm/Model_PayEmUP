349df4321b986413cc8d27493f5ec931d18dd136
Mauascm <mauricioivanascenciom@gmail.com>
2023-05-31 09:31:26 -0600

    NuevoModelo

--- testModel.py
+++ testModel.py
@@ -1,29 +1,66 @@
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.feature_selection import RFE
-from sklearn.impute import SimpleImputer
-from sklearn.metrics import r2_score
+from sklearn.linear_model import LogisticRegression
 
-# Read the dataset
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error, r2_score
+
+
+
+import numpy as np
+
+import tensorflow as tf
+
+### analisis breve de los datos contenidos en la base de datos ### 
+
 url = 'https://raw.githubusercontent.com/Mauascm/Model_PayEmUP/main/Salary2USACleaned.csv'
 data = pd.read_csv(url)
 
-# Take a sample of the data for a quick test
-data = data.sample(frac=0.01, random_state=42)
+#### tomando solamente una muestra de los datos para tener una prueba rápida.
+data = data.sample(frac=0.05, random_state=42)
 
-# Define the socio-demographic and academic columns
+# Ver las primeras filas de los datos
+print(data.head())
+
+# Ver información general sobre los datos
+print(data.info())
+
+# Ver estadísticas descriptivas de las variables numéricas
+print(data.describe())
+
+# Ver la cantidad de valores únicos en cada columna
+print(data.nunique())
+
+# Ver la cantidad de valores nulos en cada columna
+print(data.isnull().sum())
+
+# Eliminar las filas con valores nulos
+data = data.dropna()
+
+
+### ---------------------------- ###
+
+from sklearn.preprocessing import PolynomialFeatures
+
+# Definir las columnas socio-demográficas y académicas
 socio_demographic_cols = ['CASE_STATUS', 'EMPLOYER_NAME', 'PREVAILING_WAGE_SUBMITTED', 'PREVAILING_WAGE_SUBMITTED_UNIT', 'WORK_CITY', 'WORK_STATE', 'FULL_TIME_POSITION_Y_N', 'VISA_CLASS']
 academic_cols = ['PREVAILING_WAGE_SOC_CODE', 'PREVAILING_WAGE_SOC_TITLE', 'JOB_TITLE_SUBGROUP']
 
-# Create the transformers for numeric and categorical columns
+# Crear los transformadores para las columnas numéricas y categóricas
 num_transformer = StandardScaler()
 cat_transformer = OneHotEncoder(drop='first', handle_unknown='ignore')
 
-# Create a preprocessor that applies the transformations to the corresponding columns
+# Crear un transformador polinomial
+poly_transformer = PolynomialFeatures(degree=2)
+
+# Crear un preprocesador que aplique las transformaciones a las columnas correspondientes
 preprocessor_socio_demographic = ColumnTransformer(
     transformers=[
         ('num', num_transformer, ['PREVAILING_WAGE_SUBMITTED']),
@@ -34,48 +71,70 @@ preprocessor_academic = ColumnTransformer(
         ('num', num_transformer, []),
         ('cat', cat_transformer, academic_cols)])
 
-# Create pipelines that apply the preprocessor, feature selection, and fit the model
-pipeline_socio_demographic = Pipeline(steps=[
-    ('preprocessor', preprocessor_socio_demographic),
-    ('imputer', SimpleImputer(strategy='mean')),
-    ('feature_selection', RFE(RandomForestRegressor(n_estimators=10, random_state=42))),
-    ('regressor', RandomForestRegressor(n_estimators=10, random_state=42))
-])
+# Crear un pipeline que aplique el preprocesador, el transformador polinomial y luego ajuste el modelo
+pipeline_socio_demographic = Pipeline(steps=[('preprocessor', preprocessor_socio_demographic),
+                                              ('poly', poly_transformer),
+                                              ('regressor', LinearRegression())])
 
-pipeline_academic = Pipeline(steps=[
-    ('preprocessor', preprocessor_academic),
-    ('imputer', SimpleImputer(strategy='mean')),
-    ('feature_selection', RFE(RandomForestRegressor(n_estimators=10, random_state=42))),
-    ('regressor', RandomForestRegressor(n_estimators=10, random_state=42))
-])
+pipeline_academic = Pipeline(steps=[('preprocessor', preprocessor_academic),
+                                    ('poly', poly_transformer),
+                                    ('regressor', LinearRegression())])
 
-# Split the data into training and test sets
-X_train_socio_demographic, X_test_socio_demographic, y_train_socio_demographic, y_test_socio_demographic = train_test_split(
-    data[socio_demographic_cols], data['PAID_WAGE_PER_YEAR'], test_size=0.2, random_state=42)
+# La variable objetivo es el salario pagado
+salary = data['PAID_WAGE_PER_YEAR']
 
-X_train_academic, X_test_academic, y_train_academic, y_test_academic = train_test_split(
-    data[academic_cols], data['PAID_WAGE_PER_YEAR'], test_size=0.2, random_state=42)
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train_socio_demographic, X_test_socio_demographic, y_train_socio_demographic, y_test_socio_demographic = train_test_split(data[socio_demographic_cols], salary, test_size=0.2, random_state=42)
+X_train_academic, X_test_academic, y_train_academic, y_test_academic = train_test_split(data[academic_cols], salary, test_size=0.2, random_state=42)
 
-# Fit the models
+# Ajustar el pipeline y predecir los salarios
+pipeline_socio_demographic.fit(X_train_socio_demographic, y_train_socio_demographic)
+y_pred_socio_demographic = pipeline_socio_demographic.predict(X_test_socio_demographic)
+
+pipeline_academic.fit(X_train_academic, y_train_academic)
+y_pred_academic = pipeline_academic.predict(X_test_academic)
+
+print('Datos preprocesados y divididos en conjuntos de entrenamiento y prueba.')
+
+# Entrenar los modelos
 pipeline_socio_demographic.fit(X_train_socio_demographic, y_train_socio_demographic)
 pipeline_academic.fit(X_train_academic, y_train_academic)
 
-# Make predictions
-y_pred_socio_demographic_train = pipeline_socio_demographic.predict(X_train_socio_demographic)
-y_pred_socio_demographic_test = pipeline_socio_demographic.predict(X_test_socio_demographic)
+# Transformar los datos de prueba
+X_test_socio_demographic_transformed = pipeline_socio_demographic.named_steps['preprocessor'].transform(X_test_socio_demographic)
+X_test_socio_demographic_transformed = pipeline_socio_demographic.named_steps['poly'].transform(X_test_socio_demographic_transformed)
 
-y_pred_academic_train = pipeline_academic.predict(X_train_academic)
-y_pred_academic_test = pipeline_academic.predict(X_test_academic)
+X_test_academic_transformed = pipeline_academic.named_steps['preprocessor'].transform(X_test_academic)
+X_test_academic_transformed = pipeline_academic.named_steps['poly'].transform(X_test_academic_transformed)
 
-# Evaluate the models using R2 score
-train_score_socio_demographic = r2_score(y_train_socio_demographic, y_pred_socio_demographic_train)
-test_score_socio_demographic = r2_score(y_test_socio_demographic, y_pred_socio_demographic_test)
+# Predecir los salarios
+y_pred_socio_demographic = pipeline_socio_demographic.named_steps['regressor'].predict(X_test_socio_demographic_transformed)
+y_pred_academic = pipeline_academic.named_steps['regressor'].predict(X_test_academic_transformed)
 
-train_score_academic = r2_score(y_train_academic, y_pred_academic_train)
-test_score_academic = r2_score(y_test_academic, y_pred_academic_test)
+# Calcular el error cuadrático medio
+mse_socio_demographic = mean_squared_error(y_test_socio_demographic, y_pred_socio_demographic)
+mse_academic = mean_squared_error(y_test_academic, y_pred_academic)
 
-print(f'R2 score of the socio-demographic model on the training set: {train_score_socio_demographic:.2f}')
-print(f'R2 score of the socio-demographic model on the test set: {test_score_socio_demographic:.2f}')
+print('Error cuadrático medio para el modelo socio-demográfico:', mse_socio_demographic)
+print('Error cuadrático medio para el modelo académico:', mse_academic)
 
-print(f'R2 score of the academic model on the training set: {train_score_academic:.2f}')
-print(f'R2 score of the academic model on the test set: {test_score_academic:.2f}')
+# Calcular RMSE
+rmse_socio_demographic = np.sqrt(mse_socio_demographic)
+rmse_academic = np.sqrt(mse_academic)
+
+# Calcular MAE
+mae_socio_demographic = mean_absolute_error(y_test_socio_demographic, y_pred_socio_demographic)
+mae_academic = mean_absolute_error(y_test_academic, y_pred_academic)
+
+# Calcular R²
+r2_socio_demographic = r2_score(y_test_socio_demographic, y_pred_socio_demographic)
+r2_academic = r2_score(y_test_academic, y_pred_academic)
+
+print('RMSE para el modelo socio-demográfico:', rmse_socio_demographic)
+print('RMSE para el modelo académico:', rmse_academic)
+
+print('MAE para el modelo socio-demográfico:', mae_socio_demographic)
+print('MAE para el modelo académico:', mae_academic)
+
+print('R² para el modelo socio-demográfico:', r2_socio_demographic)
+print('R² para el modelo académico:', r2_academic)
