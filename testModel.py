e56c6b14661ec74c3d6c603e7e8c6ff79debda56
Mauascm <mauricioivanascenciom@gmail.com>
2023-06-06 23:38:00 -0600

    Base limpiada con Daco

--- testModel.py
+++ testModel.py
@@ -12,6 +12,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error, r2_score
 
+import tensorflow as tf
 
 
 import numpy as np
@@ -20,11 +21,11 @@ import tensorflow as tf
 
 ### analisis breve de los datos contenidos en la base de datos ### 
 
-url = 'https://raw.githubusercontent.com/Mauascm/Model_PayEmUP/main/Salary2USACleaned.csv'
+url = 'https://raw.githubusercontent.com/Mauascm/Model_PayEmUP/main/Salary2USA.csv'
 data = pd.read_csv(url)
 
 #### tomando solamente una muestra de los datos para tener una prueba rápida.
-data = data.sample(frac=0.05, random_state=42)
+data = data.sample(frac=0.01, random_state=42)
 
 # Ver las primeras filas de los datos
 print(data.head())
@@ -42,43 +43,76 @@ print(data.nunique())
 print(data.isnull().sum())
 
 # Eliminar las filas con valores nulos
-data = data.dropna()
+##data = data.dropna()
+
+# Reemplazar los valores NA con la moda de la columna
+for column in data.columns:
+    data[column].fillna(data[column].mode()[0], inplace=True)
 
 
 ### ---------------------------- ###
 
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+"""
+Columnas socio-demográficas:
+
+EMPLOYER_NAME
+WORK_CITY
+COUNTRY_OF_CITIZENSHIP
+WORK_STATE
+WORK_POSTAL_CODE
+FULL_TIME_POSITION_Y_N
+VISA_CLASS
+Columnas académicas o profesionales:
+
+PREVAILING_WAGE_SUBMITTED
+PREVAILING_WAGE_SUBMITTED_UNIT
+PAID_WAGE_SUBMITTED
+PAID_WAGE_SUBMITTED_UNIT
+JOB_TITLE
+EDUCATION_LEVEL_REQUIRED
+COLLEGE_MAJOR_REQUIRED
+EXPERIENCE_REQUIRED_Y_N
+EXPERIENCE_REQUIRED_NUM_MONTHS
+PREVAILING_WAGE_SOC_CODE
+PREVAILING_WAGE_SOC_TITLE
+PREVAILING_WAGE_PER_YEAR
+PAID_WAGE_PER_YEAR
+JOB_TITLE_SUBGROUP
+
+"""
+
+#socio_demographic_cols = ['CASE_STATUS', 'EMPLOYER_NAME', 'PREVAILING_WAGE_SUBMITTED', 'PREVAILING_WAGE_SUBMITTED_UNIT', 'WORK_CITY', 'WORK_STATE', 'FULL_TIME_POSITION_Y_N', 'VISA_CLASS']
+#academic_cols = ['PREVAILING_WAGE_SOC_CODE', 'PREVAILING_WAGE_SOC_TITLE', 'JOB_TITLE_SUBGROUP']
 
 # Definir las columnas socio-demográficas y académicas
-socio_demographic_cols = ['CASE_STATUS', 'EMPLOYER_NAME', 'PREVAILING_WAGE_SUBMITTED', 'PREVAILING_WAGE_SUBMITTED_UNIT', 'WORK_CITY', 'WORK_STATE', 'FULL_TIME_POSITION_Y_N', 'VISA_CLASS']
-academic_cols = ['PREVAILING_WAGE_SOC_CODE', 'PREVAILING_WAGE_SOC_TITLE', 'JOB_TITLE_SUBGROUP']
+socio_demographic_cols = ['EMPLOYER_NAME', 'WORK_CITY', 'COUNTRY_OF_CITIZENSHIP', 'WORK_STATE', 'WORK_POSTAL_CODE', 'FULL_TIME_POSITION_Y_N', 'VISA_CLASS']
+academic_cols = ['PREVAILING_WAGE_SUBMITTED', 'PREVAILING_WAGE_SUBMITTED_UNIT', 'PAID_WAGE_SUBMITTED', 'PAID_WAGE_SUBMITTED_UNIT', 'JOB_TITLE', 'EDUCATION_LEVEL_REQUIRED', 'COLLEGE_MAJOR_REQUIRED', 'EXPERIENCE_REQUIRED_Y_N', 'EXPERIENCE_REQUIRED_NUM_MONTHS', 'PREVAILING_WAGE_SOC_CODE', 'PREVAILING_WAGE_SOC_TITLE', 'PREVAILING_WAGE_PER_YEAR', 'PAID_WAGE_PER_YEAR', 'JOB_TITLE_SUBGROUP']
+
 
 # Crear los transformadores para las columnas numéricas y categóricas
 num_transformer = StandardScaler()
 cat_transformer = OneHotEncoder(drop='first', handle_unknown='ignore')
 
-# Crear un transformador polinomial
-poly_transformer = PolynomialFeatures(degree=2)
-
 # Crear un preprocesador que aplique las transformaciones a las columnas correspondientes
 preprocessor_socio_demographic = ColumnTransformer(
     transformers=[
-        ('num', num_transformer, ['PREVAILING_WAGE_SUBMITTED']),
         ('cat', cat_transformer, socio_demographic_cols)])
 
 preprocessor_academic = ColumnTransformer(
     transformers=[
-        ('num', num_transformer, []),
+        ('num', num_transformer, ['PREVAILING_WAGE_SUBMITTED']),
         ('cat', cat_transformer, academic_cols)])
 
 # Crear un pipeline que aplique el preprocesador, el transformador polinomial y luego ajuste el modelo
 pipeline_socio_demographic = Pipeline(steps=[('preprocessor', preprocessor_socio_demographic),
-                                              ('poly', poly_transformer),
-                                              ('regressor', LinearRegression())])
+                                              ('regressor', RandomForestRegressor())])
 
 pipeline_academic = Pipeline(steps=[('preprocessor', preprocessor_academic),
-                                    ('poly', poly_transformer),
-                                    ('regressor', LinearRegression())])
+                                    ('regressor', RandomForestRegressor())])
+
 
 # La variable objetivo es el salario pagado
 salary = data['PAID_WAGE_PER_YEAR']
@@ -87,29 +121,15 @@ salary = data['PAID_WAGE_PER_YEAR']
 X_train_socio_demographic, X_test_socio_demographic, y_train_socio_demographic, y_test_socio_demographic = train_test_split(data[socio_demographic_cols], salary, test_size=0.2, random_state=42)
 X_train_academic, X_test_academic, y_train_academic, y_test_academic = train_test_split(data[academic_cols], salary, test_size=0.2, random_state=42)
 
-# Ajustar el pipeline y predecir los salarios
+print('Datos preprocesados y divididos en conjuntos de entrenamiento y prueba.')
+
+
+# Entrenar los modelos
 pipeline_socio_demographic.fit(X_train_socio_demographic, y_train_socio_demographic)
 y_pred_socio_demographic = pipeline_socio_demographic.predict(X_test_socio_demographic)
 
 pipeline_academic.fit(X_train_academic, y_train_academic)
 y_pred_academic = pipeline_academic.predict(X_test_academic)
-
-print('Datos preprocesados y divididos en conjuntos de entrenamiento y prueba.')
-
-# Entrenar los modelos
-pipeline_socio_demographic.fit(X_train_socio_demographic, y_train_socio_demographic)
-pipeline_academic.fit(X_train_academic, y_train_academic)
-
-# Transformar los datos de prueba
-X_test_socio_demographic_transformed = pipeline_socio_demographic.named_steps['preprocessor'].transform(X_test_socio_demographic)
-X_test_socio_demographic_transformed = pipeline_socio_demographic.named_steps['poly'].transform(X_test_socio_demographic_transformed)
-
-X_test_academic_transformed = pipeline_academic.named_steps['preprocessor'].transform(X_test_academic)
-X_test_academic_transformed = pipeline_academic.named_steps['poly'].transform(X_test_academic_transformed)
-
-# Predecir los salarios
-y_pred_socio_demographic = pipeline_socio_demographic.named_steps['regressor'].predict(X_test_socio_demographic_transformed)
-y_pred_academic = pipeline_academic.named_steps['regressor'].predict(X_test_academic_transformed)
 
 # Calcular el error cuadrático medio
 mse_socio_demographic = mean_squared_error(y_test_socio_demographic, y_pred_socio_demographic)
@@ -138,6 +158,7 @@ print('MAE para el modelo académico:', mae_academic)
 
 print('R² para el modelo socio-demográfico:', r2_socio_demographic)
 print('R² para el modelo académico:', r2_academic)
+
 
 import gym
 from gym import spaces
@@ -196,13 +217,13 @@ class JobChangeEnv(gym.Env):
     def reset(self):
     # Reset the state of the environment to an initial state
         self.current_index = 0
-        return np.array([self.data.iloc[self.current_index]['PAID_WAGE_PER_YEAR'], 0, 0])
+        return np.array(self.data.iloc[self.current_index]['PAID_WAGE_PER_YEAR'])
 
 
     def render(self, mode='human'):
         pass
 
-    import tensorflow as tf
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
@@ -223,7 +244,7 @@ class DQNAgent:
 
     def _build_model(self):
         model = Sequential()
-        model.add(Dense(24, input_shape=self.state_size, activation='relu'))
+        model.add(Dense(24, input_shape=(1,), activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
@@ -242,7 +263,7 @@ class DQNAgent:
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
-            state = np.array(state).astype('float32')
+            state = np.reshape(state, [1, state_size])
             next_state = np.array(next_state).astype('float32')
             target = self.model.predict(np.expand_dims(state, axis=0))
             if done:
@@ -254,13 +275,17 @@ class DQNAgent:
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
-
 actions = list(pipeline_academic.named_steps.keys())
-state_size = (3,)
+state_size = (1,)
 action_size = len(actions)
 agent = DQNAgent(state_size, action_size)
 done = False
 batch_size = 32
+
+#define environment
+env = JobChangeEnv(data, pipeline_socio_demographic, pipeline_academic)
+max_steps_per_episode = 10
+
 
 n_episodes = 1000  # Set the desired number of episodes
 
